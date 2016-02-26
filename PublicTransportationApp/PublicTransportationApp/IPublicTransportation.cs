67a8278e3a0b6d36a296ae82e8ed17bab5e4d862
MonicaStefania <monik_shtefania@yahoo.com>
2016-02-26 20:31:26 +0100

    Start

---
+++ PublicTransportationApp/PublicTransportationApp/IPublicTransportation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicTransportationApp
+{
+   public interface IPublicTransportation
+    {
+         void Attach(IPublicTransportation obj);
+         void Detach(IPublicTransportation obj);
+        void Notify(IPublicTransportation obj);
+        bool isDelayed { get; set; }
+        string From { get; set; }
+        string To { get; set; }
+        int Hour { get; set; }
+        int Minutes { get; set; }
+         int Delay { get; set; }
+        string State(IPublicTransportation obj);
+    }
+}
---
+++ PublicTransportationApp/PublicTransportationApp/Train.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicTransportationApp
+{
+    class Train : IPublicTransportation
+    {
+        List<Train> myTrains;
+        
+        public Train(String from, string to, int h, int min, int delay, bool isDeplayed)
+        {
+            this.From = from;
+            this.To = to;
+            this.Hour = h;
+            this.Minutes = min;
+            this.Delay = delay;
+            this.isDelayed = isDelayed;
+        }
+
+        public int Delay { get; set; }
+
+        public bool isDelayed { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Hour { get; set; }
+        public int Minutes { get; set; }
+      
+
+        public void Attach(PublicTransportation obj)
+        {
+            myTrains.Add((Train)obj);
+            Notify(obj);
+        }
+
+        public void Detach(PublicTransportation obj)
+        {
+            this.myTrains.Remove((Train)obj);
+        }
+
+        public void Notify(PublicTransportation obj)
+        {
+            if(obj.isDelayed)
+            {
+                //do smth
+            }
+        }
+        public string State(PublicTransportation obj)
+        {
+            return "";
+        }
+    }
+}
