6cfad7ab657e3519ab25a16c8b101e0353e5f20c
MonicaStefania <monik_shtefania@yahoo.com>
2016-03-26 23:52:51 +0100

    Just classes and methods
    No implementation of the methods

---
+++ AdapterPattern/AdapterPattern/GameAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class GameAdapter
+    {
+        IIos ios;
+        GameAdapter(IIos ios) { }
+        string TransmitGame(String info) { return ""; }
+        string TransformGame(String info) { return ""; }
+    }
+}
---
+++ AdapterPattern/AdapterPattern/Iphone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Iphone : IIos
+    {
+        public string RunIosGame(string info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
